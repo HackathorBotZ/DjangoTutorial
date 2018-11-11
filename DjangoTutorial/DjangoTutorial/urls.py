9f64815dfbccbbf587c2b6afd766d08300a770f2
Jeff Zhao <bote.zhao@hotmail.com>
2018-11-11 19:57:36 -0400

    Create Initial Django App

--- DjangoTutorial/DjangoTutorial/urls.py
+++ DjangoTutorial/DjangoTutorial/urls.py
@@ -1,6 +1,8 @@
 """
 Definition of urls for DjangoTutorial.
 """
+from django.conf.urls import include, url
+import HelloDjangoApp.views
 
 from django.conf.urls import include, url
 
@@ -18,4 +20,6 @@ urlpatterns = [
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', HelloDjangoApp.views.index, name='index'),
+    url(r'^home$', HelloDjangoApp.views.index, name='home'),
 ]

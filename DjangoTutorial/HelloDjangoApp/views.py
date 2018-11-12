4e3774c896ce3accadad78789906b567a1b54048
Jeff Zhao <bote.zhao@hotmail.com>
2018-11-11 23:08:16 -0400

    Rendered a view using HTML

--- DjangoTutorial/HelloDjangoApp/views.py
+++ DjangoTutorial/HelloDjangoApp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from datetime import datetime
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+
+    html_content = "<html><head><title>Hello, Django</title></head><body>"
+    html_content += "<strong>Hello Django!</strong> on " + now.strftime("%A, %d %B, %Y at %X")
+    html_content += "</body></html>"
+    return HttpResponse(html_content)
